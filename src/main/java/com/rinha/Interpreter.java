3516d1c98502e2c1981bb20adf8c7ab912bcc3f5
Jottinha <joaovitorfps852@gmail.com>
2023-09-17 14:13:16 -0300

    Add Fibonacci function and some binary operation

--- src/main/java/com/rinha/Interpreter.java
+++ src/main/java/com/rinha/Interpreter.java
@@ -1,48 +1,98 @@
 package com.rinha;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Interpreter {
 
     public Object runInterpreter(JsonObject mappedJson,  Map<String, Object> env){
-        System.out.println( "Kind = "+ mappedJson.get("kind").getAsString());
+// Using this print to test some entry
+//        System.out.println( "Kind = "+ mappedJson.get("kind").getAsString());
         switch (mappedJson.get("kind").getAsString()){
             case "Print": {
                 Object printValue = runInterpreter(mappedJson.getAsJsonObject("value"), env);
                 System.out.println(printValue);
+                return printValue;
             }
 
             case "Str": {
                 return mappedJson.get("value").getAsString();
             }
 
+            case "Int": {
+                return mappedJson.get("value").getAsInt();
+            }
+
             case "Let": {
                 Object valueLet = runInterpreter(mappedJson.getAsJsonObject("value"), env);
                 env.put(mappedJson.getAsJsonObject("name").get("text").getAsString(), valueLet);
-                return runInterpreter(mappedJson.getAsJsonObject("next"), env);
-            }
-
-            case "Call": {
-                System.out.println("a");
-                if ("fib".equals(mappedJson.getAsJsonObject("callee").get("text").getAsString())){
-                    //Call mapped Json passing argumnets
-                    break;
-                }else {
-
-                }
-            }
-            case "Binary": {
-                //TODO: Finish bynary operation and strat implemnets fib function
-                //TODO: Roda o programa vou conseguir ver onde parou no fluxo
+                Map<String, Object> copyEnv = new HashMap<>(env);
+                copyEnv.put(mappedJson.getAsJsonObject("name").get("text").getAsString(), valueLet);
+                return runInterpreter(mappedJson.getAsJsonObject("next"), copyEnv);
             }
 
             case "Function": {
                 return mappedJson;
             }
 
+            case "Call": {
+                //Fibonacci simple implementation
+                if ("fib".equals(mappedJson.getAsJsonObject("callee").get("text").getAsString())){
+                    BigInteger valueFibonacci = new BigInteger(String.valueOf(runInterpreter(mappedJson.getAsJsonArray("arguments").get(0).getAsJsonObject(), env)));
+
+                    BigInteger a = BigInteger.ZERO, b = BigInteger.ONE;
+                    for (int i = 0; i < valueFibonacci.intValue(); i++){
+                        BigInteger next = a.add(b);
+                        a = b;
+                        b = next;
+                    }
+                    return a;
+                }
+            }
+
+            case "Binary": {
+                Object firstValue = runInterpreter(mappedJson.get("lhs").getAsJsonObject(), env);
+                Object secondValue = runInterpreter(mappedJson.get("rhs").getAsJsonObject(), env);
+
+                if("Str".equals(mappedJson.getAsJsonObject("lhs").get("kind").getAsString())
+                        || "Str".equals(mappedJson.getAsJsonObject("rhs").get("kind").getAsString())){
+                    return firstValue.toString().concat(secondValue.toString());
+                }
+
+                if ("Add".equals(mappedJson.get("op").getAsString())){
+                    return String.valueOf( Integer.parseInt(firstValue.toString()) + Integer.parseInt(secondValue.toString()));
+                }
+
+                if ("Sub".equals(mappedJson.get("op").getAsString())){
+                    return String.valueOf( Integer.parseInt(firstValue.toString()) - Integer.parseInt(secondValue.toString()));
+                }
+
+                if ("Mul".equals(mappedJson.get("op").getAsString())){
+                    return String.valueOf( Integer.parseInt(firstValue.toString()) * Integer.parseInt(secondValue.toString()));
+                }
+
+                if ("Div".equals(mappedJson.get("op").getAsString())){
+                    return String.valueOf( Integer.parseInt(firstValue.toString()) / Integer.parseInt(secondValue.toString()));
+                }
+
+                if ("Rem".equals(mappedJson.get("op").getAsString())){
+                    return String.valueOf( Integer.parseInt(firstValue.toString()) % Integer.parseInt(secondValue.toString()));
+                }
+
+                if ("Eq".equals(mappedJson.get("op").getAsString())){
+                    return firstValue == secondValue ? true : false;
+                }
+
+                if ("Neq".equals(mappedJson.get("op").getAsString())){
+                    return firstValue != secondValue ? true : false;
+                }
+            }
         }
         return null;
     }
